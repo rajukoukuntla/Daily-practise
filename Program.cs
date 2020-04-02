23698cef75dfe5ea49410efcf1463bf0fc19d11e
rajukoukuntla <59805746+rajukoukuntla@users.noreply.github.com>
2020-04-02 00:12:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            spaces();
+
+            test();
+
+            filedetails();
+            AnagramTest();
+            Program isa = new Program();
+            Console.WriteLine(isa.model);
+            int[] arr = { 1, 2, 9, 5, 7 };
+            //sort
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + "");
+            }
+            Array.Reverse(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + "");
+            }
+            Thread.Sleep(100000);
+        }
+
+        public static void spaces()
+        {
+            int st = 0;
+            string str = "dot net is my goal";
+            //Array sti = str.ToArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                //string str1 = str.Substring(i, 1);
+                string sti = str[i].ToString();
+                if (sti == " ")
+                {
+                    st++;
+                }
+
+                /*if()*/
+            }
+            Console.WriteLine(st);
+        }
+
+        public static void test()
+        { 
+        
+        
+        }
+
+        public static bool isAnagram(string Firstword, string secondword)
+        {
+            //if (string.IsNullOrWhiteSpace(Firstword) || string.IsNullOrWhiteSpace(secondword) || Firstword.Length != secondword.Length)
+            //{
+            //    return false;
+            //}
+            //sczfsafsdfsd
+            Firstword = Firstword.ToLower();
+            secondword = secondword.ToLower();
+
+            if (Firstword.Equals(secondword))
+                return false;
+
+            char[] Firstchar = Firstword.ToCharArray();
+            char[] Secondchar = secondword.ToCharArray();
+            Array.Sort(Firstchar);
+            Array.Sort(Secondchar);
+            for (int i = 0; i < Firstchar.Length; i++)
+            {
+                if (Firstchar[i] != Secondchar[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static void AnagramTest()
+        {
+            //Anagrams
+            Debug.Assert(isAnagram("cinema", "iceman"));
+            Debug.Assert(isAnagram("Dave Barry", "Ray Adverb"));
+            Debug.Assert(isAnagram("debit card", "bad credit"));
+            Debug.Assert(isAnagram("abc123", "c3b2a1"));
+
+            //Not Anagrams
+
+            Debug.Assert(!isAnagram("bananab", "abanana"));
+            Debug.Assert(!isAnagram("Tom Riddle", "I’m Lord Voldemort?"));
+            Debug.Assert(!isAnagram("abc123", "a11b22c33"));
+
+        }
+        public static void check()
+        {
+            //terinary method
+            int time = 20;
+            string result = (time < 18) ? "good" : "good";
+            Console.WriteLine(result);
+        }
+
+
+        public string model;
+        public void program()
+        {
+            model = "ford";
+        }
+
+        public static void filedetails()
+        {
+            string text = "data";
+            File.WriteAllText("dd.txt", text);
+            string read = File.ReadAllText("dd.txt");
+
+            Console.WriteLine(read);
+        }
+
+    }
+}
---
+++ linq.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class linq
+    {
+        class student
+        {
+            public int studentid { get; set; }
+            public string studentname { get; set; }
+            public int age { get; set; }
+            public int standardid { get; set; } 
+        }
+
+        class standard
+        {
+            public int standardid { get; set; }
+            public string standardname { get; set; }
+        }
+
+
+      
+
+        static void main(string[] args)
+        {
+            student[] students = {
+                               
+                               new student() {studentid = 1,studentname="uday",age=20},
+                               new student() {studentid = 2,studentname="uday",age=21},
+                               new student() {studentid = 3,studentname="uday",age=22},
+                               new student() {studentid = 4,studentname="uday",age=23}
+                               };
+
+            student[] stu = students.Where(s => s.studentid > 2).ToArray();
+            student bill = students.Where(s => s.studentname == "").FirstOrDefault();
+            //oftype
+
+            var sftype = from s in students.OfType<string>() select s;
+            var ss = from s in students orderby s.studentname descending select s;
+            var thenby = students.OrderBy(s => s.studentname).ThenBy(s => s.age);
+            
+        }
+
+        public static void das ()
+        {
+            IList<string> stringlist = new List<string>() { "a", "b" };
+
+            //query method with where
+            var stu = from s in stringlist where s.Contains("a") select s;
+            var sti = stringlist.Where(s => s.Contains("a"));
+        }
+
+        public static void testinsert()
+        {
+
+            var studentnamefirst = "";
+            var studentidfirst = "";
+
+
+            IList<student> studentlist = new List<student>()
+            {
+                new student{studentname="raju",studentid=Convert.ToInt32(studentidfirst)}
+            };
+
+            var strin = from s in studentlist where s.studentid == 4 where s.studentname == "" select s.studentname;
+            student studentl = studentlist.Where(s => s.studentid == 4).FirstOrDefault();
+            student[] studentd = studentlist.Where(s => s.studentid > 4 && s.studentid < 2).ToArray();
+            var studentde = studentlist.Where(s => s.studentid > 4 && s.studentid < 2);
+        }
+
+        public static void join()
+        {
+            List<student> studentlists = new List<student>();
+            List<standard> standardlidt = new List<standard>();
+            var join = studentlists.Join(standardlidt, student => student.standardid, standard => standard.standardid, (student, standard) => new { });
+        }
+
+    }
+}
